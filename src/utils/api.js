3dfe089c3b18ec369aaa369dae9d3db53aa35187
Rahul Kumar <88238469+notinrange@users.noreply.github.com>
2025-02-14 02:26:14 +0530

    Update api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 // const API_BASE_URL = 'http://localhost:8000'; // Adjust if necessary
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:8000";,
+  baseURL: process.env.REACT_APP_API_BASE_URL,
 });
 
 api.interceptors.request.use(
